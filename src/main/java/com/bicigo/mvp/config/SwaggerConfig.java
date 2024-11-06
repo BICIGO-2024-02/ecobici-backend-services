a13577c0730b3fa242f391020c059808ad98e2a9
Sofia Alfaro <alfaros.sofia@gmail.com>
2024-11-06 16:58:18 -0500

    fix bearer authentication

---
+++ src/main/java/com/bicigo/mvp/config/SwaggerConfig.java
@@ -0,0 +1,25 @@
+package com.bicigo.mvp.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@OpenAPIDefinition(
+        info = @Info(title = "BiciGo API", version = "v1"),
+        security = @SecurityRequirement(name = "Bearer Authentication")
+)
+@SecurityScheme(
+        name = "Bearer Authentication",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        scheme = "bearer"
+)
+public class SwaggerConfig {
+    String schemeName = "bearerAuth";
+    String bearerFormat = "JWT";
+    String scheme = "bearer";
+}
